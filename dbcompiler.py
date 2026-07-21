25fceb0cf75ccf157ab828d5865920c22639c585
Jimmxyz <Jimmynoux@protonmail.com>
2026-07-21 14:30:11 +0200

    v1.3.0 -> Started to support 4 more language (English, Spanish, Italian and Japanese) -> Added lang to showcase -> Fixed a bug with the db compiler UPDATED QUIZ : -> Canada provinces and capitals in English, spanish, italian and japanese. -> USA (English, Spanish, Italian and Japanese) -> AUstralia and NZ : English -> UK and UKC : English -> Italy : English, Spanish, Italian, Japanese -> Megacity (5 lang) -> Spain : English, Spanish, Italian, Japanese -> Japan

--- dbcompiler.py
+++ dbcompiler.py
@@ -6,10 +6,10 @@ import json
 file_list = [os.path.join(dirpath, file) for (dirpath, dirnames, filenames) in os.walk("./content") for file in filenames]
 file_list = [i.replace('\\', '/').removeprefix('./content/') for i in file_list]
 
-with open('dbinfo.json', 'r') as f:
+with open('dbinfo.json', 'r', encoding='utf-8') as f:
     data = json.load(f)
 
 data['QUIZ_LIST'] = file_list
 
-with open('dbinfo.json', 'w') as f:
-    json.dump(data, f, indent=4)
+with open('dbinfo.json', 'w', encoding='utf-8') as f:
+    json.dump(data, f, indent=4, ensure_ascii=False)
